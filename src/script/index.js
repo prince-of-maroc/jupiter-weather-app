55e4b3c88c2013b41c1d9f4534895ca38bd4a6be
Rodrick <jesprikovic@gmail.com>
2022-10-04 11:13:06 -0400

    Add rudimentary ability to use form to search for temperatures

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,19 @@
 import getTemperature from "./modules/get-temperature.js";
 import getWeatherData from "./modules/get-weather-data.js";
+
+getWeatherData("F", "new york city").then((data) => getTemperature(data));
+
+const input = document.querySelector("input");
+const cityName = document.querySelector("h1");
+const tempDisplay = document.querySelector("p");
+
+async function displayInfo() {
+    cityName.innerText = input.value;
+    tempDisplay.innerText = await getWeatherData("F", input.value).then(
+        (data) => getTemperature(data)
+    );
+}
+
+document.querySelector("button").addEventListener("click", () => {
+    displayInfo();
+});
