365c9f669351217f9be44201a790fd32339e7bb8
Rodrick <jesprikovic@gmail.com>
2022-10-05 12:01:51 -0400

    Remove redundant test code

--- src/script/index.js
+++ src/script/index.js
@@ -1,21 +1,4 @@
 import "../style/style.scss";
-import getTemperature from "./modules/get-temperature.js";
-import getWeatherData from "./modules/get-weather-data.js";
 import setImages from "./modules/set-images.js";
 
 setImages();
-
-const input = document.querySelector("input");
-const cityName = document.querySelector("h1");
-const tempDisplay = document.querySelector("p");
-
-async function displayInfo() {
-    cityName.innerText = input.value;
-    tempDisplay.innerText = await getWeatherData("F", input.value).then(
-        (data) => getTemperature(data)
-    );
-}
-
-document.querySelector("button").addEventListener("click", () => {
-    displayInfo();
-});
