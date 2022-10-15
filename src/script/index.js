075325ff11019421ed731d186865366decb0cd7a
Rodrick <jesprikovic@gmail.com>
2022-10-15 11:44:47 -0400

    Add live time-refresh

--- src/script/index.js
+++ src/script/index.js
@@ -13,16 +13,27 @@ const temp = document.querySelector(".weather h1");
 const extraData = document.querySelector(".weather h2");
 const time = document.querySelector(".time");
 
+let offset = 0;
+
+function setTime() {
+    time.innerText = getCityTime(offset);
+    setTimeout(function () {
+        setTime();
+    }, 1000);
+}
+setTime();
+
 searchbar.addEventListener("keypress", (e) => {
     if (e.key == "Enter") {
         getWeatherData("F", searchbar.value).then((obj) => {
+            time.style.opacity = 1;
             console.log(obj);
             city.innerText = obj.name;
             getTemperature(obj).then((t) => (temp.textContent = t));
             extraData.innerText = `Expect ${Math.round(
                 obj.wind.speed
             )}mph ${getDirection(obj.wind.deg)} winds today.`;
-            time.innerText = getCityTime(obj.timezone * 1000);
+            offset = obj.timezone * 1000;
         });
         searchbar.value = "";
         e.preventDefault();
