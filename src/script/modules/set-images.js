473543ea04f12c554c4844f27613013c80d41836
Rodrick <jesprikovic@gmail.com>
2022-10-17 16:16:27 -0400

    Re-organize cards in HTML

--- src/script/modules/set-images.js
+++ src/script/modules/set-images.js
@@ -1,4 +1,4 @@
-import JupiterIMGSource from "../../assets/jupiter.png";
+import JupiterIMGSource from "../../assets/icons/jupiter.png";
 
 export default function setImages() {
     document.querySelector(".jupiter").src = JupiterIMGSource;
