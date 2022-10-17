f208d3a26da4088347442a7794b4701c10a8467b
Rodrick <jesprikovic@gmail.com>
2022-10-17 16:31:49 -0400

    Add card child elements

--- src/script/modules/set-images.js
+++ src/script/modules/set-images.js
@@ -1,5 +1,8 @@
 import JupiterIMGSource from "../../assets/icons/jupiter.png";
+import OvercastIMGSource from "../../assets/icons/overcast.png";
 
 export default function setImages() {
     document.querySelector(".jupiter").src = JupiterIMGSource;
+
+    document.querySelector("#hour1 img").src = OvercastIMGSource;
 }
