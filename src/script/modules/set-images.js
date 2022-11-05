b2ab3ebff6bbd17f33af7eb45fc4751abf360a40
Rodrick <jesprikovic@gmail.com>
2022-11-05 16:15:31 -0400

    Add work-in-progress image to detail view

--- src/script/modules/set-images.js
+++ src/script/modules/set-images.js
@@ -1,5 +1,7 @@
 import JupiterIMGSource from "../../assets/icons/jupiter.png";
+import inProgress from "../../assets/art/work-in-progress.png";
 
 export default function setImages() {
     document.querySelector(".jupiter").src = JupiterIMGSource;
+    document.querySelector(".progress").src = inProgress;
 }
